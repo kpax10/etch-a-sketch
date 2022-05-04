28d4796f39e8b0cc193c424964812f510642ac80
Kyle <notxap55@yahoo.com>
2022-05-04 15:56:50 -0700

    continue

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const container = document.querySelector('.container');
+
+let square = document.createElement('div');
+square.classList.add('square');
+container.append(square);
+
+// square = document.createElement('div');
+// square.classList.add('square');
+// container.append(square);
+
+let containerSize = 16; // or .value
+
+for (let i = 0; i < containerSize; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    container.append(square);
+}
