af2e5d35d5d3d8f800102b1413fd7d8955fbc56e
Kyle <notxap55@yahoo.com>
2022-05-06 14:59:45 -0700

    button take in new length
    removal of box not working

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 'use strict';
 
 const container = document.querySelector('.container');
+let boxLength = 16;
+const getSize = document.querySelector('.prompt');
 
-let containerSize = 16; // or prompt * 4?
+const generateBox = function () {
+    for (let i = 0; i < boxLength ** 2; i++) {
+        const square = document.createElement('div');
 
-for (let i = 0; i < containerSize ** 2; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `${600 / containerSize}px`;
-    container.append(square);
+        square.classList.add('square');
+        square.style.width = `${600 / boxLength}px`;
+        // square.style.height = `${600 / boxLength}px`;
+        container.append(square);
+    };
 };
+
+const removeBox = function () {
+    for (let i = 0; i < boxLength ** 2; i++) {
+
+        square.classList.remove('square');
+
+        container.remove(square);
+    };
+};
+
+generateBox();
+
+const squares = document.querySelectorAll('.square');
+// forEach square, add event listener hover to change color to black
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.classList.add('black');
+    });
+});
+
+getSize.addEventListener('click', () => {
+    boxLength = prompt('How many squares per side?');
+    boxLength = Number(boxLength);
+
+    squares.forEach((square) => {
+        square.classList.remove('black');
+    });
+    // removeBox();
+    generateBox();
+});
+
