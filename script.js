9e4d195a8fc3f2ef9ce089d4367a0098c34ab4f3
Kyle <notxap55@yahoo.com>
2022-05-05 22:05:39 -0700

    continue

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 
 const container = document.querySelector('.container');
 
-let square = document.createElement('div');
-square.classList.add('square');
-container.append(square);
+// let square = document.createElement('div');
+// square.classList.add('square');
+// container.append(square);
 
 // square = document.createElement('div');
 // square.classList.add('square');
 // container.append(square);
 
-let containerSize = 16; // or .value
+let containerSize = 256; // or .value
 
 for (let i = 0; i < containerSize; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     container.append(square);
 }
+
