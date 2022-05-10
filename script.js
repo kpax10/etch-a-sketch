4fb112ca26bd2b48bc5e10a5238f3c3971eb4bc8
Kyle <notxap55@yahoo.com>
2022-05-10 13:47:57 -0700

    box displaying properly

--- script.js
+++ script.js
@@ -4,41 +4,33 @@ const container = document.querySelector('.container');
 let boxLength = 16;
 const getSize = document.querySelector('.prompt');
 
-const generateBox = function () {
+const displayBox = function () {
     for (let i = 0; i < boxLength ** 2; i++) {
         let createSquare = document.createElement('div');
         createSquare.classList.add('square');
         createSquare.style.width = `${600 / boxLength}px`;
         container.appendChild(createSquare);
     };
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('black');
+        });
+    });
 };
 
 const removeBox = function () {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
-
-        // DOES THE ELEMENT NEED TO BE WIPED OF ITS PROPERTIES COMPLETELY?
-        // REMOVE EVENT LISTENER??
     };
 };
 
-generateBox();
-
-const squares = document.querySelectorAll('.square');
-
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.classList.add('black');
-    });
-});
-
+displayBox();
 
 getSize.addEventListener('click', () => {
     boxLength = prompt('How many squares per side?');
     boxLength = Number(boxLength);
-
     removeBox();
-    generateBox();
-
+    displayBox();
 });
 
