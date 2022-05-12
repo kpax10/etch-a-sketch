b606d8df0b05b2c16213ca37a52fcdf55984640c
Kyle <notxap55@yahoo.com>
2022-05-11 22:19:08 -0700

    tab spacing

--- script.js
+++ script.js
@@ -9,70 +9,70 @@ let boxLength = 16;
 let color = 'black'
 
 const getColorCode = () => {
-	const makeColorCode = '0123456789ABCDEF';
-	let color = '#';
-	for (var count = 0; count < 6; count++) {
-		color = color + makeColorCode[Math.floor(Math.random() * 16)];
-	};
-	return color;
+  const makeColorCode = '0123456789ABCDEF';
+  let color = '#';
+  for (var count = 0; count < 6; count++) {
+    color = color + makeColorCode[Math.floor(Math.random() * 16)];
+  };
+  return color;
 };
 
 const displayBox = function () {
-	for (let i = 0; i < boxLength ** 2; i++) {
-		let createSquare = document.createElement('div');
-		createSquare.classList.add('square');
-		createSquare.style.width = `${600 / boxLength}px`;
-		container.appendChild(createSquare);
-	};
-	let squares = document.querySelectorAll('.square');
-	squares.forEach((square) => {
-		square.addEventListener('mouseover', () => {
-			square.style.backgroundColor = color;  //getColorCode();  //This makes a rainbow
-		});
-	});
+  for (let i = 0; i < boxLength ** 2; i++) {
+    let createSquare = document.createElement('div');
+    createSquare.classList.add('square');
+    createSquare.style.width = `${600 / boxLength}px`;
+    container.appendChild(createSquare);
+  };
+  let squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = color;  //getColorCode();  //This makes a rainbow
+    });
+  });
 };
 
 const removeBox = function () {
-	while (container.firstChild) {
-		container.removeChild(container.firstChild);
-	};
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  };
 };
 
 displayBox();
 
 getSize.addEventListener('click', () => {
-	boxLength = Number(prompt('How many squares per side?'));
-	if (!((boxLength >= 1) && (boxLength <= 100))) {
-		boxLength = Number(prompt('Needs to be a number between 1 - 100'));
-	};
-	removeBox();
-	displayBox();
+  boxLength = Number(prompt('How many squares per side?'));
+  if (!((boxLength >= 1) && (boxLength <= 100))) {
+    boxLength = Number(prompt('Needs to be a number between 1 - 100'));
+  };
+  removeBox();
+  displayBox();
 });
 
 randomColor.addEventListener('click', () => {
-	let squares = document.querySelectorAll('.square');
-	squares.forEach((square) => {
-		square.addEventListener('mouseover', () => {
-			square.style.opacity = 1;
-			square.style.backgroundColor = getColorCode();  //getColorCode();  //This makes a rainbow
-		});
-	});
+  let squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.opacity = 1;
+      square.style.backgroundColor = getColorCode();  //getColorCode();  //This makes a rainbow
+    });
+  });
 });
 
 gradient.addEventListener('click', () => {
-	removeBox();
-	displayBox();
-	let squares = document.querySelectorAll('.square');
-	squares.forEach((square) => {
-		let counter = 0;
-		square.addEventListener('mouseover', () => {
-			counter += 0.1;
-			square.style.opacity = counter;
-		});
-	});
+  removeBox();
+  displayBox();
+  let squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    let counter = 0;
+    square.addEventListener('mouseover', () => {
+      counter += 0.1;
+      square.style.opacity = counter;
+    });
+  });
 });
 
 clear.addEventListener('click', () => {
-	removeBox();
-	displayBox();
+  removeBox();
+  displayBox();
 });
